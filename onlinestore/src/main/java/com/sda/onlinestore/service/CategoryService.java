8fddaee6e65931a0ac7049040c41eb5bed64244f
AntonioCarneluti <antonio.carneluti91@gmail.com>
2020-10-11 10:52:50 +0300

    added new methods to category service

--- onlinestore/src/main/java/com/sda/onlinestore/service/CategoryService.java
+++ onlinestore/src/main/java/com/sda/onlinestore/service/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -41,5 +42,39 @@ public class CategoryService {
             categoryDtoList.add(categoryDto);
         }
         return categoryDtoList;
+    }
+
+    public void updateCategory(CategoryDto categoryDto){
+        Optional<CategoryModel> optionalCategoryModel = categoryRepository.findById(categoryDto.getId());
+        if(optionalCategoryModel.isPresent()){
+            CategoryModel categoryModel = new CategoryModel();
+            categoryModel.setName(categoryDto.getName());
+            categoryRepository.save(categoryModel);
+        }
+    }
+
+    private CategoryDto findCategoryById(Long id){
+        Optional<CategoryModel> optionalCategoryModel = categoryRepository.findById(id);
+        if (optionalCategoryModel.isPresent()){
+            CategoryModel categoryModel = optionalCategoryModel.get();
+            CategoryDto categoryDto = new CategoryDto();
+            categoryDto.setId(categoryModel.getId());
+            categoryDto.setName(categoryModel.getName());
+            List<CategoryModel> children = categoryModel.getChildren();
+            List<CategoryDto> childrenDto = new ArrayList<>();
+            for(CategoryModel child: children){
+                CategoryDto childDto = new CategoryDto();
+                childDto.setId(child.getId());
+                childDto.setName(child.getName());
+                childrenDto.add(childDto);
+            }
+            categoryDto.setChildren(childrenDto);
+            return categoryDto;
+        }
+        return null;
+    }
+
+    public void deleteCategory(Long id){
+        categoryRepository.deleteById(id);
     }
 }
