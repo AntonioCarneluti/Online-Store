f206c2c542c5ab7e746ac360ddd6afac2df84814
iulianaDumitru <iulymincea@gmail.com>
2020-10-11 11:18:31 +0300

    com13

--- onlinestore/src/main/java/com/sda/onlinestore/service/OrderService.java
+++ onlinestore/src/main/java/com/sda/onlinestore/service/OrderService.java
@@ -2,14 +2,17 @@ package com.sda.onlinestore.service;
 
 import com.sda.onlinestore.dto.OrderDto;
 import com.sda.onlinestore.dto.OrderLineDto;
+import com.sda.onlinestore.dto.ProductDto;
 import com.sda.onlinestore.model.OrderLineModel;
 import com.sda.onlinestore.model.OrderModel;
+import com.sda.onlinestore.model.ProductModel;
 import com.sda.onlinestore.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -18,16 +21,62 @@ public class OrderService {
     private OrderRepository orderRepository;
 
 
-    public void addOrder(OrderDto orderDto){
+    /*public void addOrder(OrderDto orderDto){
         OrderModel orderModel = new OrderModel();
-        orderModel.setId(orderDto.getId());
         orderModel.setTotalCost(orderDto.getTotalCost());
-        List<OrderLineDto> orderLineDto = orderDto.getOrderLineDtoModels();
-        List<OrderLineModel> orderLineModels = new ArrayList<>();
-        for(OrderLineDto orderLineDto1: orderLineDto){
 
-        }
+        }*/
 
         //orderModel.setOrderLineModels(orderDto.getOrderLineDtoModels());
+
+
+    private List<OrderDto> getOrders(){
+        List<OrderModel> orderModels = orderRepository.findAll();
+        List<OrderDto> orderDtos = new ArrayList<>();
+
+        for (OrderModel orderModel: orderModels){
+            OrderDto orderDto = new OrderDto();
+            orderDto.setId(orderModel.getId());
+            orderDto.setTotalCost(orderModel.getTotalCost());
+
+            List<OrderLineModel> orderLineModels = orderModel.getOrderLineModels();
+            List<OrderDto> orderDtos1 = new ArrayList<>();
+
+            for(OrderLineModel orderLineModel: orderLineModels){
+                OrderLineDto orderLineDto = new OrderLineDto();
+                orderLineDto.setId((orderLineModel.getId()));
+                orderLineDto.setPrice(orderLineModel.getPrice());
+                orderLineDto.setQuantity(orderLineModel.getQuantity());
+
+                ProductModel productModel = orderLineModel.getProductModel();
+                ProductDto productDto = new ProductDto();
+
+                productDto.setId(productModel.getId());
+                productDto.setPrice(productModel.getPrice());
+                productDto.setName(productModel.getName());
+
+                orderLineDto.setProductDto(productDto);
+                orderDtos1.add(orderDto);
+            }
+
+            orderDtos.add(orderDto);
+        }
+
+        return orderDtos;
     }
+
+    public OrderDto findById(Long id){
+        Optional<OrderModel> orderModel = orderRepository.findById(id);
+        OrderDto orderDto = new OrderDto();
+        if(orderModel.isPresent()){
+            OrderModel foundOrderModel = orderModel.get();
+            orderDto.setId(foundOrderModel.getId());
+            orderDto.setTotalCost(foundOrderModel.getTotalCost());
+
+            return  orderDto;
+        }
+        return  null;
+    }
+
+    
 }
